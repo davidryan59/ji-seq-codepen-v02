6fc70c3d5877d76b740d5acddd4ae546887f1355
David Ryan <davidryan1998@hotmail.com>
2019-11-13 14:40:36 +0700

    Sequencer now uses linear ramps

--- index.js
+++ index.js
@@ -5,73 +5,81 @@
 
 (function() {
 
-  const beatsPerMinute = 260;
+  // Main constants
+  const beatsPerMinute = 300
   const mainGainDb = -10
+  const minGainDb = -200
+  const maxBeats = 256
+  const maxFreqHz = 22050
 
+  // Timbre constants - MOVE THESE to a per-instrument basis
+  const resFreq1_Hz = 300
+  const resFreq2_Hz = 80
+
+  // Controls number of channels, and their sound
+  const channelSetupArray = [
+    {channelGainDb: -3, type: 'triangle'},
+    {channelGainDb: -50, type: 'triangle'},
+  ]
+
+  // Sequencing of how parameters of channel change over time, using LINEAR ramping.
   // Each sub-array is a single note of format:
   // [ channelNum, noteLengthBeats, frequencyHz, noteGainDb ]
-  // Channel starts 1, 2, ...
-  // Default values are:
-  // [ 1, 0, 0, 0]
-  // To play a rest, set frequency to zero
-  const dt = 0.1
+  // channelNum 1, 2, 3... is mandatory, all the rest are optional
+  // noteLengthBeats is number 0 < x <= 256
+  // frequencyHz is any number -22050 <= x <= 22050
+  // noteGainDb is any number -800 <= x <= 800
+  const dt = 0.0001
   const sequencedData = [
-    [1, dt],   // Start gap
-    [2, dt],
+    [2, dt, 440, minGainDb],
 
-    [1, 1],
-    [1, 1, 180],
-    [1, 1, 160],
-    [1, 1, 150],
-    [1, 1, 140],
-    [1, 1, 130],
-    [1, 1, 120],
-    [1, 1, 110],
-    [1, 4, 100],
-
-    [2, 2, 200],
-    [2, 2, 250],
-    [2, 1, 300],
-    [2, 1, 350],
-    [2, 1, 450],
-    [2, 1, 425],
-    [2, 6, 400],
-
-    [1, dt],   // End gap
-    [2, dt],
+    [1, dt, 400, minGainDb],
+    [1, 1, 400, 0],
+    [1, 1, 400],
+    [1, 1, 500],
+    [1, 1, 500],
+    [1, 1, 600],
+    [1, 1, 600],
+    [1, 1, 900],
+    [1, 1, 900],
+    [1, 1, 800],
+    [1, 1, 300],
+    [1, 1, 400],
+    [1, 1, 400],
+    [1, 1, 400],
+    [1, 4, 400, minGainDb],
   ]
 
-  const triOsc = {type: 'triangle'}
-  const sineOsc = {type: 'sine'}
-  const sqOsc = {type: 'square'}
-  const sawOsc = {type: 'sawtooth'}
-  const chipEnv = {attack: 0.001, decay: 0.0001, sustain: 1, release: 0.01}
-  const slowEnv = {attack: 0.2, decay: 0.2, sustain: 0.5, release: 1}
-
-  const channelSetupArray = [
-    {channelGainDb: 0, harmonicity: 1, modulationIndex: 5,
-      oscillator: triOsc, envelope: chipEnv, modulation: triOsc, modulationEnvelope: chipEnv},
-    {channelGainDb: -3, harmonicity: 2, modulationIndex: 3,
-      oscillator: triOsc, envelope: chipEnv, modulation: triOsc, modulationEnvelope: chipEnv},
-  ]
-  // Bug in FMSynth - oscillator and modulation appear not to work...
-
-  const minGain = -200
-  const gainChangeMs = 500
-  const timeConstantS = 0.001 * gainChangeMs
   Tone.Transport.bpm.value = beatsPerMinute;
-  let channelToneArray = [];
+  const maxChannel = channelSetupArray.length
+  const maxGainToTestDb = -4 * minGainDb    // Give more space for supplied gain values
+  let tonesToStart = []
+  let tonesToDispose = []
   let tonesSetup = false
 
   function setupTones() {
     console.log('Setting up tones')
-    channelToneArray = []
+    tonesToStart = []
+    tonesToDispose = []
     channelSetupArray.forEach(channelSetup => {
-      const newSource = new Tone.Oscillator(channelSetup).toMaster()
-      newSource.volume = minGain
-      channelToneArray.push(newSource)
+      // Create tones
+      const newSource = new Tone.Oscillator(channelSetup)
+      const theDelayTime1_s = 0.5 / resFreq1_Hz
+      const theDelayTime2_s = 0.5 / resFreq2_Hz
+      const newDelay1 = new Tone.Delay(theDelayTime1_s, theDelayTime1_s)
+      const newDelay2 = new Tone.Delay(theDelayTime2_s, theDelayTime2_s)
+      const newDelayGain = new Tone.Gain(-0.5)
+      // Link tones
+      newSource.chain(Tone.Master)
+      newSource.chain(newDelay1, newDelayGain, Tone.Master)
+      newSource.chain(newDelay2, newDelayGain, Tone.Master)
+      // Store tones
+      tonesToStart.push(newSource)
+      tonesToDispose.push(newSource)
+      tonesToDispose.push(newDelay1)
+      tonesToDispose.push(newDelay2)
+      tonesToDispose.push(newDelayGain)
     })
-    tonesSetup = true
   }
 
   function sequenceTones() {
@@ -79,56 +87,70 @@
 
     const chanStartBeatArray = []
 
-    sequencedData.forEach( data => {
-      const channelNum = (data[0] || 1)
-      const noteLenBeats = data[1] || 0
-      const noteFreqHz = data[2] || 0
-      const noteGainDb = noteFreqHz ? data[3] || 0 : minGain
+    sequencedData.forEach( rowData => {
 
-
+      // Deal with channel
+      const channelNum = rowData[0]     // Should be 1 ... maxChannel
+      if (!Number.isInteger(channelNum) || channelNum < 1 || maxChannel < channelNum) return
       const channelIndex = channelNum - 1
+      // Could factor this out into an array?
       const channelSetup = channelSetupArray[channelIndex]
       const channelGainDb = channelSetup ? channelSetup.channelGainDb || 0 : 0
+      console.log(`Channel ${channelNum} with index ${channelIndex} and gain ${channelGainDb}`)
 
-      const noteFinalGainDb = mainGainDb + channelGainDb + noteGainDb
-      const noteStartBeats = chanStartBeatArray[channelIndex] || 0
-      const noteEndBeats = noteStartBeats + noteLenBeats
-      chanStartBeatArray[channelIndex] = noteEndBeats
-
-      const noteAmplitude = Math.pow(10, 0.05 * noteFinalGainDb)
-      const noteStartTxt = `+0:${noteStartBeats}:0`
+      // Deal with timing
+      const noteLenBeats = rowData[1]   // Should be positive number <= maxBeats
+      if (!Number.isFinite(noteLenBeats) || noteLenBeats <= 0 || maxBeats < noteLenBeats) return
       const noteLenTxt = `0:${noteLenBeats}:0`
+      const noteStartBeat = chanStartBeatArray[channelIndex] || 0
+      const noteStartTxt = `+0:${noteStartBeat}:0`
+      const noteEndBeat = noteStartBeat + noteLenBeats
+      chanStartBeatArray[channelIndex] = noteEndBeat
+      console.log(`- Beat start ${noteStartBeat} length ${noteLenBeats} end ${noteEndBeat}`)
 
-      const theTone = channelToneArray[channelIndex]
+      // Later on, the parameters might be spread out over more than 1 tone...
+      const theTone = tonesToStart[channelIndex]
       if (theTone) {
-        theTone.frequency.setValueAtTime(noteFreqHz, noteStartTxt)
-        theTone.volume.linearRampTo(noteFinalGainDb, timeConstantS, noteStartTxt)
-      }
-      // if (theTone) theTone.triggerAttackRelease(noteFreqHz, noteLenTxt, noteStartTxt, noteAmplitude)
 
-      console.log(`Sequencing note on channel ${channelNum} from ${noteStartBeats.toFixed(2)} to ${noteEndBeats.toFixed(2)} beats, at ${noteFreqHz.toFixed(2)}Hz, with gain ${noteFinalGainDb.toFixed(1)} (amplitude ${noteAmplitude.toFixed(4)})`)
+        // Deal with frequency changes
+        const noteFreqHz = rowData[2]
+        if (Number.isFinite(noteFreqHz) && -maxFreqHz <= noteFreqHz && noteFreqHz <= maxFreqHz) {
+          theTone.frequency.linearRampTo(noteFreqHz, noteLenTxt, noteStartTxt)
+          console.log(`- - Linear frequency ramp to ${noteFreqHz}`)
+        }
+
+        // Deal with gain changes
+        const noteGainDb = rowData[3]
+        if (Number.isFinite(noteGainDb) && -maxGainToTestDb <= noteGainDb && noteGainDb <= maxGainToTestDb) {
+          const totalGainDb = mainGainDb + channelGainDb + noteGainDb
+          theTone.volume.linearRampTo(totalGainDb, noteLenTxt, noteStartTxt)
+          console.log(`- - Linear gain ramp to ${totalGainDb}`)
+        }
+      }
     })
   }
 
   function startTones() {
     console.log('Starting tones')
-    channelToneArray.forEach( tone => tone.start() )
+    tonesToStart.forEach( tone => tone.start() )
   }
 
   function disposeTones() {
     console.log('Disposing of tones')
-    channelToneArray.forEach( tone => tone.dispose() )
-    channelToneArray = []
-    tonesSetup = false
+    tonesToDispose.forEach( tone => tone.dispose() )
+    tonesToDispose = []
+    tonesToStart = []
   }
 
   function toggleTones() {
     if (tonesSetup) {
       disposeTones()
+      tonesSetup = false
     } else {
       setupTones()
       sequenceTones()
       startTones()
+      tonesSetup = true
     }
   }
 
